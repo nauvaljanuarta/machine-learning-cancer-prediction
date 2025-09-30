71a34ed688d9875faa44afad868f914554e82f3d
ndajatoh01 <nauvalaryanto2005@gmail.com>
2025-09-30 10:32:12 +0700

    menambahkan deskripsi dari setiap data yang sudah di proses

--- main.py
+++ main.py
@@ -9,58 +9,76 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import VarianceThreshold, SelectKBest, f_classif
 from imblearn.over_sampling import SMOTE
 
-# ========== PERSIAPAN DATA ==========
+
 df = pd.read_csv("risk_factors_cervical_cancer.csv")
 
-# Ganti '?' dan kosong jadi NaN
+# mengganti string anomali jadi NaN
 df.replace(['?', ' ', ''], np.nan, inplace=True)
-
-# Konversi ke numerik
+# konversi ke numerik
 df = df.apply(pd.to_numeric, errors='coerce')
+print("Jumlah NaN tiap kolom:\n", df.isna().sum().sum())
 
-# Isi NaN dengan median
+# ganti NaN dengan median
 df.fillna(df.median(), inplace=True)
+print("\npenggantian nan dengan median")
+print("data nan menjadi : \n", df.isna().sum().sum())
 
 target_col = 'Biopsy'
-
-# Encode target jika perlu
 le = LabelEncoder()
 df[target_col] = le.fit_transform(df[target_col])
+print("\nmapping ke kelas target")
+print("distribusi kelas:\n", df[target_col].value_counts())
 
+# pembagian targrt dan fitur 
 X = df.drop(target_col, axis=1)
 y = df[target_col]
+print("data feature")
+print(X.columns.to_list)
 
-# Scaling
+# normalisasi minmax
 scaler = MinMaxScaler()
 X_scaled = scaler.fit_transform(X)
+print("\ndata setelah normalisasi minmax")
+print (X_scaled)
 
-# Hilangkan fitur constant
+# menghilangkan fitur konstan
 var_thresh = VarianceThreshold(threshold=0.0)
 X_var = var_thresh.fit_transform(X_scaled)
+mask_var = var_thresh.get_support()
+selected_features_var = X.columns[mask_var]
 
-# Feature selection ANOVA
+#= ANOVA
 selector = SelectKBest(score_func=f_classif, k=10)
 X_selected = selector.fit_transform(X_var, y)
+mask_anova = selector.get_support()
+final_features = selected_features_var[mask_anova]
+print("\nfitur terpilih dari seleksi ANOVA:\n", final_features)
+print("\ndata fitur hasil seleksi ANOVA:\n", X_selected)
 
-# Balancing dengan SMOTE
+
+# balancing data dengan SMOTE
 smote = SMOTE(random_state=42)
 X_res, y_res = smote.fit_resample(X_selected, y)
+print("\ndistribusi kelas setelah SMOTE:")
+print(pd.Series(y_res).value_counts())
 
-# Split data
+# split data training dan test
 X_train, X_test, y_train, y_test = train_test_split(
     X_res, y_res, test_size=0.2, random_state=1, stratify=y_res
 )
+print("\nJumlah data train:", X_train.shape[0])
+print("Jumlah data test:", X_test.shape[0])
 
-# ========== VISUALISASI DISTRIBUSI ==========
+# visualisasi
 fig, axes = plt.subplots(1, 2, figsize=(10, 4))
 
-# Distribusi sebelum SMOTE
+# sebelum smote 
 pd.Series(y).value_counts().sort_index().plot(kind='bar', ax=axes[0], color=['skyblue', 'salmon'])
 axes[0].set_title('Distribusi Kelas Sebelum SMOTE')
 axes[0].set_xlabel('Kelas')
 axes[0].set_ylabel('Jumlah Sampel')
 
-# Distribusi setelah SMOTE
+# setelah smote
 pd.Series(y_res).value_counts().sort_index().plot(kind='bar', ax=axes[1], color=['skyblue', 'salmon'])
 axes[1].set_title('Distribusi Kelas Setelah SMOTE')
 axes[1].set_xlabel('Kelas')
@@ -69,8 +87,7 @@ axes[1].set_ylabel('Jumlah Sampel')
 plt.tight_layout()
 plt.show()
 
-# ========== VISUALISASI DATA TRAIN ==========
-# Mengambil 2 fitur terbaik dari hasil ANOVA untuk divisualisasikan
+# visualisasi fitur dari seleksi anova
 X_train_anova = X_train[:, :2]  # ambil 2 kolom pertama dari fitur hasil seleksi ANOVA
 
 plt.figure(figsize=(6, 5))
